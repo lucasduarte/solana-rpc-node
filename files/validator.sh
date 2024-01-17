349564520e874b8dbeee9d0244bb3af9ce5e0a69
CC-Horizon <113700330+CC-Horizon@users.noreply.github.com>
2024-01-17 14:38:29 +0100

    Update validator.sh

--- files/validator.sh
+++ files/validator.sh
@@ -2,6 +2,11 @@
 export SOLANA_METRICS_CONFIG="host=https://metrics.solana.com:8086,db=mainnet-beta,u=mainnet-beta_write,p=password"
 exec /mnt/solana/target/release/solana-validator \
 --identity /home/solana/rpc_node.json \
+--log /mnt/logs/solana-validator.log \
+--accounts /mnt/solana-accounts \
+--ledger /mnt/solana-ledger \
+--snapshots /mnt/solana-snapshots \
+--no-snapshot-fetch \
 --entrypoint entrypoint.mainnet-beta.solana.com:8001 \
 --entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
 --entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
@@ -10,38 +15,28 @@ exec /mnt/solana/target/release/solana-validator \
 --rpc-port 8899 \
 --dynamic-port-range 8002-8099 \
 --no-port-check \
---halt-on-trusted-validators-accounts-hash-mismatch \
 --gossip-port 8001 \
 --no-voting \
---private-rpc \
 --rpc-bind-address 0.0.0.0 \
 --enable-cpi-and-log-storage \
---account-index program-id \
+--account-index program-id spl-token-owner \
 --enable-rpc-transaction-history \
+--enable-rpc-bigtable-ledger-storage \
 --wal-recovery-mode skip_any_corrupted_record \
---log /mnt/logs/solana-validator.log \
---accounts /mnt/solana-accounts \
---ledger /mnt/solana-ledger \
---snapshots /mnt/solana-snapshots \
---expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
 --limit-ledger-size 400000000 \
 --rpc-send-default-max-retries 3 \
 --rpc-send-service-max-retries 3 \
---rpc-send-retry-ms 2000 \
+--rpc-send-retry-ms 1000 \
+--tpu-enable-udp \
+--rpc-send-leader-count 3 \
 --full-rpc-api \
---accounts-index-memory-limit-mb 350 \
 --account-index-exclude-key kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6 \
---tpu-use-quic \
---known-validator PUmpKiNnSVAZ3w4KaFX6jKSjXUNHFShGkXbERo54xjb \
+--known-validator 7cVfgArCheMR6Cs4t6vz5rfnqd56vZq4ndaBrY5xkxXy \
+--known-validator DDnAqxJVFo2GVTujibHt5cjevHMSE9bo8HJaydHoshdp \
 --known-validator Ninja1spj6n9t5hVYgF3PdnYz2PLnkt7rvaw3firmjs \
---known-validator ChorusmmK7i1AxXeiTtQgQZhQNiXYU84ULeaYF1EH15n \
+--known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
+--known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
+--known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
 --known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
---known-validator SerGoB2ZUyi9A1uBFTRpGxxaaMtrFwbwBpRytHefSWZ \
---known-validator FLVgaCPvSGFguumN9ao188izB4K4rxSWzkHneQMtkwQJ \
---known-validator qZMH9GWnnBkx7aM1h98iKSv2Lz5N78nwNSocAxDQrbP \
---known-validator GiYSnFRrXrmkJMC54A1j3K4xT6ZMfx1NSThEe5X2WpDe \
---known-validator LA1NEzryoih6CQW3gwQqJQffK2mKgnXcjSQZSRpM3wc \
---known-validator Certusm1sa411sMpV9FPqU5dXAYhmmhygvxJ23S6hJ24 \
---known-validator 9bkyxgYxRrysC1ijd6iByp9idn112CnYTw243fdH2Uvr \
---known-validator 12ashmTiFStQ8RGUpi1BTCinJakVyDKWjRL6SWhnbxbT \
---known-validator FdaysQ2BZWUGBy8nqFgiudnrhzJp4xChQ8B4zJdc2JZB
+--known-validator 8ebFZA8NPLBZD91CwsG1HWQsa2B5Ludgdyf5Hi3sYhhs \
+--known-validator GiYSnFRrXrmkJMC54A1j3K4xT6ZMfx1NSThEe5X2WpDe
