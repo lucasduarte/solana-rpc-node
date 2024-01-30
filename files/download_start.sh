d7bb7ad5aabad52c334d83f7341bb9bc1b78ddc5
Ali Imran <50736910+aliimran2000@users.noreply.github.com>
2024-01-30 19:56:34 +0500

    Update download_start.sh

--- files/download_start.sh
+++ files/download_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo systemctl stop sol.service
-python3 /mnt/snapshot-finder.py --snapshot_path /mnt/solana-ledger/snapshot
+python3 /mnt/snapshot-finder.py --snapshot_path /mnt/solana-ledger
 sudo systemctl start sol.service
